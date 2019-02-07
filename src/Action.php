e406a852b75fba5411c84488cfecb526f348cb7a
Alex <slexx1234@gmail.com>
2019-02-08 01:50:45 +0300

    Add __invoke magic method

--- src/Action.php
+++ src/Action.php
@@ -4,5 +4,11 @@ namespace Slexx\LaravelActions;
 
 abstract class Action
 {
-
+    /**
+     * @return mixed
+     */
+    public function __invoke()
+    {
+        return call_user_func_array([$this, 'execute'], func_get_args());
+    }
 }
